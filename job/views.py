b7aae3a670997efe9d87d683393a06edc0f49aac
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2025-05-31 02:20:44 +0300

    make styles for job_list and set it's settings

--- job/views.py
+++ job/views.py
@@ -4,8 +4,12 @@ from .models import Job
 # Create your views here.
 def job_list(request):
     job_list = Job.objects.all()
+    context = {'job_list': job_list}
 
-    return render ()
+    return render (request, 'job/job_list.html', context=context)
 
 def job_details(request, id):
-    pass
+    job_details = Job.objects.get(id=id)
+    context={'job': job_details}
+
+    return render(request, 'job/job_detail.html',context=context)
